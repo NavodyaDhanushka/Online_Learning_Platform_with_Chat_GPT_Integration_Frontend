939702ba4796b9b3ea09571c67593da81ca2de6c
Navodya <navodyadhanushka01@gmail.com>
2025-11-29 10:48:06 +0530

    update the aiService for get the userId

--- vite-project/src/services/aiService.tsx
+++ vite-project/src/services/aiService.tsx
@@ -4,8 +4,14 @@ const API_URL = import.meta.env.VITE_API_BASE_URL;
 
 
 export async function getAiCourseSuggestions(query: string) {
+    const token = localStorage.getItem("token")
+
     const res = await axios.post(`${API_URL}/ai/ask`, {
         query,
+    },{
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
     });
 
     return res.data;
